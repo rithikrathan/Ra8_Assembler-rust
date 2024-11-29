a6905a323a3750a1aeb758076772c79333f9da99
RAthan_rithik <rithikrathan.c@gmail.com>
2024-11-29 17:27:28 +0530

    idk commit

--- Example_Assembly_code/FACTORIAL.asm
+++ Example_Assembly_code/FACTORIAL.asm
@@ -3,13 +3,14 @@ $START: LDI 0x04
 MOV B A
 NOPE
 $LOOP: DCR B ;loop that does n*(n-1)
-JZ END ;this is a forward reference
+JZ $END ;this is a forward reference
 MOV C A
 MOV D B
 XOR C
 $LOOP1: ADD C ;loop that multiplies two numbers
 DCR D 
-JNZ LOOP1
-JZ LOOP
+
+JNZ $LOOP1
+JZ $LOOP
 $END: STA 0x00 ;End if completed
 HLT
